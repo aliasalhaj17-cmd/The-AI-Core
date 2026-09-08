2b25854d6423892dd327c66cc10b6fe200d6e580
aliasalhaj17-cmd <aliasalhaj17@gmail.com>
2026-09-08 04:56:16 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,12 @@
 ```javascript
-const cacheName = 'ojen-v2';
-const staticAssets = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'ojen-core-v1';
+const assets = ['./index.html', './manifest.json'];
 
-self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
 ```
-
----
